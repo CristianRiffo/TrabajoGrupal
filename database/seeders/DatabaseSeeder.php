65730401ce698c0bf84d81b47946f8e8858b2a59
unknown <cristian.riffob@correoaiep.cl>
2024-07-10 01:05:40 -0400

    Agregado en modelo Post category_id cascade default 1 y Agregado a seeders default category

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Category;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -28,6 +29,11 @@ class DatabaseSeeder extends Seeder
         $user->saveOrFail();
         $user->assignRole('admin');
 
+        $category = new Category([
+            'title' => 'Sin categoria',
+            'slug' => 'uncategorized'
+        ]);
+        $category->saveOrFail();
     }
 }
 
