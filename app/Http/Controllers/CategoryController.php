8bcb26d9b3faf2d0766b4ba05599e83235ebc528
CristianRiffo <cristian.riffob@correoaiep.cl>
2024-08-16 13:29:40 -0400

    prueba git

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -14,6 +14,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
+        //test
         $categories = Category::get();
         return view('categories.index', compact('categories'));
     }
